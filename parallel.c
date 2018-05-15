7b040ed77fa8d01949f9ecef61e4763b6b2f7058
Lakshya Srivastava <lakshyasri@outlook.com>
2018-05-15 15:28:10 -0400

    OpenMP matrix Multiplication

--- parallel.c
+++ parallel.c
@@ -1,226 +1,140 @@
-// csv file not uploaded the the generetor too, use any double set csv value with input file name for successful compilation @lakshyasri@outlook.com
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
-#include<sys/time.h>
-#include<time.h>
-#include<math.h>
-#include<string.h>
-
-struct timeval tm1, tm2;
-long long t;
-
-void timing_start() {
-    gettimeofday(&tm1, NULL);
-}
-
-void timing_stop() {
-    gettimeofday(&tm2, NULL);
-
-    t = (tm2.tv_sec - tm1.tv_sec) * 1000000 + (tm2.tv_usec - tm1.tv_usec);
-}
-
-void print_timing() {
-    printf("%lld microseconds occured\n", t);
-}
-
-long long get_timing() {
-    return t;
-}
+#include <stdlib.h>
+#include <omp.h>
+#include "timing.c"
+#include <string.h>
 
 
-// Size of Vector and Matrix.
-int row1, col1, col2, currentRow = 0;
-double **InMat1, **InMat2, **ResMat;
+double **Matrix;
+double **Matrix1;
+int size;
+double **Matrix_Mult_Result = NULL;
 
-pthread_t *threads;
+/* populating two matrices for multiplication */
+void GetMatrix(char fileName[]) {
 
-int numberOfThreads;
+    int j;
+    int i;
+    char *token;
+    size_t len = 0;
+    ssize_t read;
+    char *line = NULL;
+    int lineCount = 0;
+    char lineStr[15000];
+    int numbersCount = 0;
+    const char delim[2] = ",";
+    FILE *fp = fopen("inputMatrix150_1.csv", "rb");
 
-/* Mutex for the currentRow. */
-pthread_mutex_t mutex_Row = PTHREAD_MUTEX_INITIALIZER;
+    // Check for file pointer
+    if (fp == NULL) {
+        printf("\nError reading file");
+        exit(-1);
+    }
 
 
-void *operator(int Id) {
+    while ((read = getline(&line, &len, fp)) != -1) {
+        sprintf(lineStr, "%s", line);
 
-    int i, j, myRow;
+        numbersCount = 0;
 
-    while (1) {
+        if (lineCount == 0) {
 
-        //Locking
+            // allocate the memory
+            size = atoi(line);
+            Matrix = (double **) malloc(size * sizeof(double *));
+            Matrix1 = (double **) malloc(size * sizeof(double *));
 
-        pthread_mutex_lock(&mutex_Row);
 
-        if (currentRow >= row1) {
+            for (i = 0; i < size; i++) {
+                Matrix[i] = (double *) malloc(size * sizeof(double));
+                Matrix1[i] = (double *) malloc(size * sizeof(double));
+            }
+        } else if (lineCount > size) {
+            break;
+        } else {
 
-            pthread_mutex_unlock(&mutex_Row);
+            token = strtok(lineStr, delim);
 
-            if (Id == 0)
-
-                return 0;
-
-            pthread_exit(0);
+            while (token != NULL) {
+                sscanf(token, "%lf", &Matrix[lineCount - 1][numbersCount]);
+                sscanf(token, "%lf", &Matrix1[lineCount - 1][numbersCount]);
+                numbersCount++;
+                token = strtok(NULL, delim);
+            }
 
         }
-        myRow = currentRow;
+    /*freeing memory*/
+        if (line != NULL) {
+            free(line);
+            line = NULL;
+        }
 
-        currentRow++;
-
-        // Thread working
-
-        pthread_mutex_unlock(&mutex_Row);
-
-        // unlocking Row
-
-        for (j = 0; j < col2; j++)
-
-            for (i = 0; i < col1; i++)
-
-                ResMat[myRow][j] += InMat1[myRow][i] * InMat2[i][j];
-
-
+        lineCount++;
     }
+
+    /*close file */
+    fclose(fp);
+
 }
 
+double **Matrix;
+double **Matrix1;
+double **matrix_product = NULL;
 
+int size;
 int main(int argc, char *argv[]) {
-
-    clock_t start, end;
-    double cpu_time_used, N;
+    int i, j, k, num_threads, chunk;
+    double result = 0;
     FILE *fp;
-    srand(time(0));
-    time_t t;
-    FILE *fp1;
+    char delim[2];
+    num_threads = atoi(argv[1]);
+    chunk = 20;
 
-    /*
-       Hold for Threads.  pthread_t *threads;  int numberOfThreads;
-         */
 
-    int i, j;
+    GetMatrix("inputMatrix.csv");                 //function call to get matrix
 
-    if (argc < 1) {
 
-        printf("\n Insufficient argumets. \n Usage:");
-        printf(" exe row1 col1 col2 threads.\n");
-        return 0;
+    matrix_product = malloc(size * sizeof(double *));
+
+    if (matrix_product == NULL) {
+        printf("Memory has not been allocated");
+        return 1;
     }
-
-    numberOfThreads = abs(atoi(argv[1]));
-    N = abs(atoi(argv[2]));
-
-    row1 = N;
-    col1 = N;
-    col2 = N;
-
-    printf("\nNumber of threads: %d", numberOfThreads);
-
-    InMat1 = malloc(N * sizeof(double));
-    InMat2 = malloc(N * sizeof(double));
-    ResMat = malloc(N * sizeof(double));
-
-    for (i = 0; i < N; i++)
-        InMat1[i] = malloc(sizeof(double) * N);
-    for (i = 0; i < N; i++)
-        InMat2[i] = malloc(sizeof(double) * N);
-    for (i = 0; i < N; i++)
-        ResMat[i] = malloc(sizeof(double) * N);
-
-
-
-    /* Populate the Matrices from csv File */
-    int size = N;
-
-    char buffer[1024];
-    char *record, *line;
-    char *a;
-    int mat[100][100];
-    int temp[100];
-    FILE *fstream = fopen("inputMatrix.csv", "r");
-    if (fstream == NULL) {
-        printf("\n file opening failed ");
-        return -1;
-    }
-    while ((line = fgets(buffer, sizeof(buffer), fstream)) != NULL) {
-        record = strtok(line, ",");
-        while (record != NULL) {
-            //copy all elements of the matrix into 1d array
-            temp[i++] = atoi(record);
-            record = strtok(NULL, ",");
+    timing_start();
+/* parallel region start */
+#pragma omp parallel shared (Matrix, result, matrix_product, num_threads, chunk) private(i, j, k)
+    {
+        omp_set_num_threads(num_threads);
+#pragma omp for schedule(guided, chunk)
+        for (i = 0; i < size; i++) {
+            matrix_product[i] = (double *) malloc(size * sizeof(double));
         }
-        //prepare the matrix
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                InMat1[i][j] = temp[i * size + j];
+
+#pragma omp for reduction(+:result)
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+                result = 0;
+                for (k = 0; k < size; k++) {
+                    result = result + Matrix[i][k] * Matrix1[k][j];
+                }
+
+                matrix_product[i][j] = result;
             }
         }
-
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                InMat2[i][j] = temp[i * size + j];
-            }
-        }
-
     }
-
-    //transposing
-    for (j = 0; j < N; j++) {
-        for (i = 0; i < N; i++) {
-            double temp1;
-            temp1 = InMat2[i][j];
-            InMat2[i][j] = InMat2[j][i];
-            InMat2[j][i] = temp1;
-        }
-    }
-
-
-
-    threads = (pthread_t *) malloc(sizeof(pthread_t) * numberOfThreads);
-
-
-    /* Distribution of the work. */
-    
-    timing_start();	//time started  
-    currentRow = 0;
-
-
-    for (i = 0; i < numberOfThreads; i++) {
-
-        pthread_create(&threads[i], NULL, (void *(*)(void *)) operator, (void *) (i + 1));
-
-    }
-
-    for (i = 0; i < numberOfThreads; i++) {
-
-        pthread_join(threads[i], NULL);
-    }
-
-    printf("\n\n");
-
-
-    for (i = 1; i < N; i++) {
-
-        for (j = 0; j < N; j++)
-
-            printf("%lf ", ResMat[i][j]);
-
-        printf("\n");
-
-    }
-    timing_stop();        //time stopped
+    timing_stop();
     print_timing();
-    get_timing();
-    printf("\n We're done!!!!\n");
-    char comma[2];
 
-    fp = fopen("outMatrix.csv", "w");  // Saving to output file in csv format
-    for (i = 0; i < N; i++) {
-        sprintf(comma, "%s", "");
-        for (j = 0; j < N; j++) {
-            fprintf(fp, "%s%4lf", comma, ResMat[i][j]);
-            sprintf(comma, "%s", ",");;
+/* for storing the output */
+    fp = fopen("outputMatrix.csv", "w");
+    fprintf(fp, "%d\n", size);
+
+    for (j = 0; j < size; j++) {
+        sprintf(delim, "%s", "");
+
+        for (i = 0; i < size; i++) {
+            fprintf(fp, "%s%.2f", delim, matrix_product[j][i]);
+            sprintf(delim, "%s", ",");;
         }
         fprintf(fp, "\n");
     }
@@ -228,4 +142,13 @@ int main(int argc, char *argv[]) {
     fclose(fp);
 
 
+    for (j = 0; j < size; j++) {
+        free(matrix_product[j]);
+        free(Matrix[j]);
+    }
+/* freeing the matrix */
+    free(matrix_product);
+    free(Matrix);
+
+    return 0;
 }
